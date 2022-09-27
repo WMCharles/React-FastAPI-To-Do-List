eda5cea5fac04bc3fc5db7c915a070cd05079256
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-27 12:37:03 +0300

    fetch data from server & setData once user submits input

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TodoList from './TodoList'
 import TodoForm from './TodoForm'
 
 function ToDo() {
-  return (
-    <div class="container">
-        <div class="page-content page-container" id="page-content">
-            <div class="padding">
-                <div class="row container d-flex justify-content-center">
-                    <div class="col-md-12">
-                        <div class="card px-3">
-                            <div class="card-body">
-                                <h4 class="card-title">Awesome Todo list</h4>
-                                <TodoForm/>
-                                <TodoList/>
+
+    const [todos, setTodos] = useState([])
+
+    useEffect(
+        () => {
+            fetch("http://localhost:3000/todos")
+            .then((res) => res.json())
+            .then((items) => setTodos(items))
+        },
+        []
+    )
+    
+    //add new item and refresh component
+    function addItem(item){
+        const newTodos = [...todos, item]
+        setTodos(newTodos)
+    }
+    return (
+        <div className="container">
+            <div className="page-content page-container" id="page-content">
+                <div className="padding">
+                    <div className="row container d-flex justify-content-center">
+                        <div className="col-md-12">
+                            <div className="card px-3">
+                                <div className="card-body">
+                                    <h4 className="card-title">Awesome Todo list</h4>
+                                    <TodoForm addItem={addItem}/>
+                                    <TodoList items={todos}/>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ToDo;
