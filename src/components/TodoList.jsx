46e7939ab624a3a8c12c90f3297d5881850cddcd
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-27 19:36:05 +0300

    add function that updates the value of check box when a person clicks the box

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function TodoList({items, deleteItem}) {
+export default function TodoList({items, deleteItem, updateTodos}) {
 
     // Item delete function
     // Uses deleteItem to update the todos list (ToDo.jsx: line 26)
@@ -12,6 +12,22 @@ export default function TodoList({items, deleteItem}) {
         .then((res) => res.json())
         .then(() => deleteItem(todo))
     }
+
+    // Updating Item - Checking checkbox
+    function handleCheck(item){
+        fetch(`http://localhost:3000/todos/${item.id}`, {
+            method: "PATCH",
+            headers: {
+                "content-type":"application/json"
+            },
+            body: JSON.stringify({
+                complete: !item.complete
+            })
+        })
+        .then((res) => res.json())
+        .then((updatedItem) => updateTodos(updatedItem))
+    }
+
     return (
         <div className="list-wrapper">
             <ul className="d-flex flex-column-reverse todo-list">
@@ -19,7 +35,15 @@ export default function TodoList({items, deleteItem}) {
                     <li key={item.id}>
                         <div className="form-check">
                             <label className="form-check-label">
-                                <input className="checkbox" type="checkbox"/>{item.title}<i className="input-helper"></i>
+                                <input
+                                 className="checkbox" 
+                                 type="checkbox" 
+                                 defaultChecked={item.complete}
+                                 onClick={() => handleCheck(item)}
+                                />
+
+                                {item.title}
+                                <i className="input-helper"></i>
                             </label> 
                         </div>
                         <i className="remove mdi mdi-close-circle-outline" onClick={() => handleDelete(item)}></i>
