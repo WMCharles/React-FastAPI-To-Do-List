27ef5f647904ee17efe244a34f287de07bff9d76
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-27 13:23:17 +0300

    add delete funtionality for fetch

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
-export default function TodoList({items}) {
+export default function TodoList({items, deleteItem}) {
+
+    // Item delete function
+    // Uses deleteItem to update the todos list (ToDo.jsx: line 26)
+    // Handled by onClick event handler (line 25)
+    function handleDelete(todo){
+        fetch(`http://localhost:3000/todos/${todo.id}`, {
+            method: "DELETE"
+        })
+        .then((res) => res.json())
+        .then(() => deleteItem(todo))
+    }
     return (
         <div className="list-wrapper">
             <ul className="d-flex flex-column-reverse todo-list">
@@ -11,10 +22,9 @@ export default function TodoList({items}) {
                                 <input className="checkbox" type="checkbox"/>{item.title}<i className="input-helper"></i>
                             </label> 
                         </div>
-                        <i className="remove mdi mdi-close-circle-outline"></i>
+                        <i className="remove mdi mdi-close-circle-outline" onClick={() => handleDelete(item)}></i>
                     </li>
-                )}
-                                            
+                )}                             
             </ul>
         </div>
     )
