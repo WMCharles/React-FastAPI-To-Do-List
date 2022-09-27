b7731836429c7af1adb56b5a8404dc64f9c613a5
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-27 12:41:01 +0300

    render data from server & add prop to fetch data from ToDo component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,37 +1,19 @@
 import React from 'react'
 
-export default function TodoList() {
+export default function TodoList({items}) {
     return (
-        <div class="list-wrapper">
-            <ul class="d-flex flex-column-reverse todo-list">
-                <li>
-                    <div class="form-check">
-                        <label class="form-check-label">
-                            <input class="checkbox" type="checkbox"/> For what reason would it be advisable. <i class="input-helper"></i>
-                        </label> 
-                    </div>
-                    <i class="remove mdi mdi-close-circle-outline"></i>
-                </li>
-                <li class="completed">
-                    <div class="form-check"> 
-                        <label class="form-check-label"> 
-                            <input class="checkbox" type="checkbox" checked=""/> 
-                            For what reason would it be advisable for me to think. 
-                            <i class="input-helper"></i>
-                        </label>
-                    </div>
-                    <i class="remove mdi mdi-close-circle-outline"></i>
-                </li>
-                <li>
-                    <div class="form-check"> 
-                        <label class="form-check-label"> 
-                            <input class="checkbox" type="checkbox"/> 
-                            it be advisable for me to think about business content? 
-                            <i class="input-helper"></i>
-                        </label> 
-                    </div>
-                    <i class="remove mdi mdi-close-circle-outline"></i>
-                </li>
+        <div className="list-wrapper">
+            <ul className="d-flex flex-column-reverse todo-list">
+                {items.map((item) => 
+                    <li key={item.id}>
+                        <div className="form-check">
+                            <label className="form-check-label">
+                                <input className="checkbox" type="checkbox"/>{item.title}<i className="input-helper"></i>
+                            </label> 
+                        </div>
+                        <i className="remove mdi mdi-close-circle-outline"></i>
+                    </li>
+                )}
                                             
             </ul>
         </div>
