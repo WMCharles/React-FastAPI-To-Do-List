8a15435f19a465ff074a3e6dae6b425fc77774ba
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-27 12:39:36 +0300

    add state, onChange, onClick function that posts data to server

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function TodoForm() {
-  return (
-    <div class="add-items d-flex"> 
-        <input type="text" class="form-control todo-list-input" placeholder="What do you need to do today?"/> 
-        <button class="add btn btn-primary font-weight-bold todo-list-add-btn">Add</button> 
-    </div>
-  )
+function TodoForm({addItem}) {
+    const [input, setInput] = useState("")
+    function handleSubmit(event){
+        event.preventDefault()
+        
+        const newItem = {
+            title: input
+        }
+
+        fetch("http://localhost:3000/todos", {
+            method: "POST",
+            headers: {
+                "content-type":"application/json"
+            },
+            body: JSON.stringify(newItem)
+        })
+        .then((res) => res.json())
+        .then((item) => addItem(item))
+        
+    }
+    return (
+        <div className="add-items d-flex" onSubmit={handleSubmit}> 
+            <input type="text" onChange={(e) => setInput(e.target.value)} className="form-control todo-list-input" placeholder="What do you need to do today?"/> 
+            <button className="add btn btn-primary font-weight-bold todo-list-add-btn" onClick={handleSubmit}>Add</button> 
+        </div>
+    )
 }
 
 export default TodoForm
